cf64923b47cbb31b1baf9528d78af26e6d343029
Sujan Shrestha <sshrest37@gmail.com>
2016-09-02 21:29:36 -0400

    2nd commit

--- guessinggame.py
+++ guessinggame.py
@@ -4,19 +4,22 @@ print("Guess the number that i am thinking. Its between 1 and 20. you have six c
 think= random.randint(1,20)
 
 for i in range(1,7):
-
-	guess = int(input())
+	
 	try:
-		if guess == think:
-			print(" You pyschic you")
-			sys.exit()
-		elif guess > think:
-			print("too high")
-		elif guess < think:
-			print("too low")
-
+		guess = int(input())	
 	except ValueError:
-		print('you did not enter a valid number')
+		print('you did not enter a valid number, try again')
+		continue
+	
+	if guess == think:
+		print(" You pyschic you")
+		sys.exit()
+	elif guess > think:
+		print("too high")
+	elif guess < think:
+		print("too low")
+
+	
 
 
 print("the number was "+ str(think))
